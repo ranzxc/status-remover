a766556281ff93cdac5f57e5ae44b49c5ace6da0
ran <87838090+ranzxc@users.noreply.github.com>
2025-01-30 16:44:08 +0800

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🌸created by zee🌸", "Darling in the Franxx ‧ EP24 ‧ Never Let Me Go"];
+const statusMessages = ["created by zee", "Darling in the Franxx ‧ EP24 ‧ Never Let Me Go"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
